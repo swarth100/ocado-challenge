be79435528608df56d6397a1ccd9359906db3920
Alberto Spina <as12015@ic.ac.uk>
2017-11-11 18:36:01 +0000

    container/: Add container interface to handle dataset

---
+++ container/container.py
@@ -0,0 +1,33 @@
+import sys
+
+class Container:
+
+    def __init__(self, rule):
+        # Initialise dimensions
+        self.height = sys.maxsize
+        self.length = sys.maxsize
+        self.width = sys.maxsize
+
+        # Keep track of rules
+        self.rule = rule
+
+        self.items = []
+
+    def addItem(self, item):
+        self.items.append(item)
+
+    def getLastItemOrder(self):
+        if len(self.items) >= 1:
+            return self.items[-1].orderID
+        return 0
+
+    def getItemWeights(self):
+        res = 0
+
+        for item in self.items:
+            res += item.weight
+
+        return res
+
+
+
